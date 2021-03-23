567c305f980f6f55479de407d02c106dc50c79c3
Zorca Orcinus <vs@zorca.org>
2021-03-24 02:14:18 +0300

    add: languages

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -98,18 +98,18 @@
                     </button>
 
                     <span v-else class="inline-flex rounded-md">
-                                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                                    type="button">
-                                                {{ $page.props.user.name }}
+                      <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                            type="button">
+                        {{ $page.props.user.name }}
 
-                                                <svg class="ml-2 -mr-0.5 h-4 w-4" fill="currentColor"
-                                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                    <path clip-rule="evenodd"
-                                                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                          fill-rule="evenodd"/>
-                                                </svg>
-                                            </button>
-                                        </span>
+                        <svg class="ml-2 -mr-0.5 h-4 w-4" fill="currentColor"
+                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path clip-rule="evenodd"
+                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                  fill-rule="evenodd"/>
+                        </svg>
+                      </button>
+                    </span>
                   </template>
 
                   <template #content>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,53 +2,36 @@ require('./bootstrap');
 
 // Import modules...
 import Vue from 'vue';
-import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue';
-import { InertiaProgress } from '@inertiajs/progress';
+import {App as InertiaApp, plugin as InertiaPlugin} from '@inertiajs/inertia-vue';
+import {InertiaProgress} from '@inertiajs/progress';
 import PortalVue from 'portal-vue';
 import VueTailwind from 'vue-tailwind';
-import {
-    TInput,
-    TTextarea,
-    TSelect,
-    TRadio,
-    TCheckbox,
-    TButton,
-    TInputGroup,
-    TCard,
-    TAlert,
-    TModal,
-    TDropdown,
-    TRichSelect,
-    TPagination,
-    TTag,
-    TRadioGroup,
-    TCheckboxGroup,
-    TTable,
-    TDatepicker,
-    TToggle,
-    TDialog,
-} from 'vue-tailwind/dist/components';
+import {TButton, TInput,} from 'vue-tailwind/dist/components';
+
 const settings = {
-    't-button': {
-        component: TButton,
-    },
+  't-input': {
+    component: TInput,
+  },
+  't-button': {
+    component: TButton,
+  },
 };
 Vue.use(VueTailwind, settings);
 
-Vue.mixin({ methods: { route } });
+Vue.mixin({methods: {route}});
 Vue.use(InertiaPlugin);
 Vue.use(PortalVue);
 
 const app = document.getElementById('app');
 
 new Vue({
-    render: (h) =>
-        h(InertiaApp, {
-            props: {
-                initialPage: JSON.parse(app.dataset.page),
-                resolveComponent: (name) => require(`./Pages/${name}`).default,
-            }
-        }),
+  render: (h) =>
+    h(InertiaApp, {
+      props: {
+        initialPage: JSON.parse(app.dataset.page),
+        resolveComponent: (name) => require(`./Pages/${name}`).default,
+      }
+    }),
 }).$mount(app);
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({color: '#4B5563'});

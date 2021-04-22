7631640c1b8e29d9592ce647179450a628e5567e
Zorca Orcinus <vs@zorca.org>
2021-04-22 18:32:51 +0300

    remove: nunito font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-        './node_modules/vue-tailwind/dist/*.js'
-    ],
+  purge: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./vendor/laravel/jetstream/**/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.vue",
+    "./node_modules/vue-tailwind/dist/*.js"
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
 
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-            cursor: ['disabled'],
-        },
-    },
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+      cursor: ["disabled"]
+    }
+  },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")]
 };

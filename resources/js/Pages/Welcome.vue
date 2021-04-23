a9d7a55ebaeec8deaf0bada51de97e0f83010cd8
Zorca Orcinus <vs@zorca.org>
2021-04-23 12:25:05 +0300

    update: layout

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -29,7 +29,7 @@
       </template>
     </div>
 
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+    <div class="sm:px-6 lg:px-8">
       <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
         <span class="text-6xl font-bold text-gray-600">Monolith</span>
       </div>
